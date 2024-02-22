ea3e82e0f72af45395b0772eb5990ca8b97f3364
Mitchell Fenner <mitchfen@pm.me>
2024-02-21 20:11:36 -0500

    more simple stuff

--- main.go
+++ main.go
@@ -9,11 +9,12 @@ import (
 
 func main() {
 
-	var investmentAmount float64 = 22000
-	var returnRateAsPercent float64 = 8.0
-	var yearsHeld = 40
-	var futureValue = investmentAmount * math.Pow(1+returnRateAsPercent/100, float64(yearsHeld))
+	investmentAmount := 22000
+	returnRateAsPercent := 8.0
+	yearsHeld := 40
+	var futureValue = float64(investmentAmount) * math.Pow(1+returnRateAsPercent/100, float64(yearsHeld))
 
-	var s = fmt.Sprintf("Initial invesment: %f\nFuture value after %d years: %f", investmentAmount, yearsHeld, futureValue)
+	// Sprintf and Printf are just the same as Print and Sprint but with formatting functions included
+	var s = fmt.Sprintf("Initial invesment: %v\nFuture value after %v years: %0.2f", investmentAmount, yearsHeld, futureValue)
 	outputHelpers.PrettyPrint(s)
 }
